8eda9aac13a6cc43f2c57d6d919ca08e76998732
Sip <sipg789@gmail.com>
2023-01-20 12:08:56 -0600

    connected to database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -8,7 +9,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // mysql username
+        user: 'root',
+        password: '@q^5y28mTPdv2tTz',
+        database: 'elections'
+    },
+    console.log('Connected to the elections database.')
+);
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // default response for any other request (Not Found)
 app.use((req, res) => {
